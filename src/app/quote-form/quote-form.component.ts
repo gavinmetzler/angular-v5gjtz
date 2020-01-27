7d4929a6420acd17c3ff65bad7ec9004ba4858e3
gavinmetzler <gavinmetzler@gmail.com>
2020-01-27 18:08:15 +1000

    1 file modified

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -108,8 +108,19 @@ export class QuoteFormComponent implements OnInit {
          console.log (decodedparams);
          let receivedformdata=JSON.parse (decodedparams);
          console.log (receivedformdata);
-         //eek, fingers crossed this works
+         //Populate the array
+         //First we have to find out how many tiles there are, and how many cuts in each tile.
+         //We've already created 1 Tile with 1 Cut, so we need to addTile()
+         for (let loadtile in receivedformdata.tiles)
+         {
+           console.log ('found a tile! Its width is '.concat(loadtile.width.tostring()));
+           for (let loadcut in loadtile.cuts)
+           {
+             console.log ('found a cut!');
+           }
+         }
           this.myForm.setValue(receivedformdata);
+          //TODO: Encoding nested arrays worked fine, but decoding doesnt.
          }
        console.log (this.queryparams);
     });
